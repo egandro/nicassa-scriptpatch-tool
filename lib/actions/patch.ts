c4ed1a8f866353e67cf3510a4ef61ec2742e1ced
Harald Fielker <harald.fielker@advancedqms.com>
2018-02-05 16:57:13 +0100

    more verbose text for dead patch steps

--- lib/actions/patch.ts
+++ lib/actions/patch.ts
@@ -17,35 +17,40 @@ export class Patch {
         this.log = opts.log;
         this.outputFileName = opts.output;
 
-        if(this.log) {
+        if (this.log) {
             this.verbose = true;
         }
 
         let ws: WorkingSet = <any>null;
         try {
             ws = ScriptPatchTool.load(this.fileName);
-        } catch(err) {
+        } catch (err) {
             console.error(err);
             process.exit(-1);
         }
 
         const result = ScriptPatchTool.run(ws, this.dry, this.outputFileName);
-        if(result.patched) {
-            if(this.dry) {
-                if(this.verbose) {
+        if (result.deadPatchSteps.length > 0) {
+            if (this.verbose) {
+                console.log(result.deadPatchSteps.length + ' step(s) did not cause any change');
+            }
+        }
+        if (result.patched) {
+            if (this.dry) {
+                if (this.verbose) {
                     console.log('content of patch file "' + this.fileName + '" can be patched');
                 }
             } else {
-                if(this.verbose) {
+                if (this.verbose) {
                     console.log('content of patch file "' + this.fileName + '" patched');
                 }
             }
-            if(this.log) {
+            if (this.log) {
                 console.log(result.contentAfter);
             }
             process.exit(0);
         } else {
-            if(this.verbose) {
+            if (this.verbose) {
                 console.log('content of patch file "' + this.fileName + '" can not be patched or does not need patching');
             }
             process.exit(-1);
